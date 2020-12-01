4b37351cd017ed767f0caf87e2de672233e42932
Drew Ellingson <drew.ellingson@gmail.com>
2020-11-30 22:04:40 -0800

    day 1

---
+++ day_1/soln.py
@@ -0,0 +1,17 @@
+def p1(nums):
+    x, y = [(x, y) for x in nums for y in nums if x + y == 2020][0]
+    return x * y
+
+
+def p2(nums):
+    x, y, z = [
+        (x, y, z) for x in nums for y in nums for z in nums if x + y + z == 2020
+    ][0]
+    return x * y * z
+
+
+if __name__ == "__main__":
+    with open("input.txt") as my_file:
+        nums = [int(x.strip()) for x in my_file.readlines()]
+    print(p1(nums))
+    print(p2(nums))

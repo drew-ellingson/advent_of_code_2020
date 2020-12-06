3085de40c83bdb3d13c63ca118b20ca7da94015f
Drew Ellingson <drew.ellingson@gmail.com>
2020-12-05 21:37:00 -0800

    cleanup

--- day_06/soln.py
+++ day_06/soln.py
@@ -1,19 +1,22 @@
 def parse(in_file):
-    groups = in_file.read().split('\n\n')
+    groups = in_file.read().split("\n\n")
     return groups
 
 
 def p1(groups):
-    group_yes = [group.replace('\n','') for group in groups] # read in all answers together
+    group_yes = [group.replace("\n", "") for group in groups]  # read answers together
     group_yes_dedup = [set(group) for group in group_yes]
     return sum([len(yes) for yes in group_yes_dedup])
 
 
 def p2(groups):
-    groups = [group.split('\n') for group in groups] # answers per group member
-    cands = range(ord('a'), ord('z') + 1)
-    all_yes = [all(chr(val) in person for person in group) for group in groups for val in cands]
+    groups = [group.split("\n") for group in groups]  # answers per group member
+    cands = range(ord("a"), ord("z") + 1)
+    all_yes = [
+        all(chr(val) in person for person in group) for group in groups for val in cands
+    ]
     return sum(all_yes)
+
 
 if __name__ == "__main__":
     with open("input.txt") as my_file:

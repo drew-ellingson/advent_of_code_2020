312abe8fd4cebd418814d177f80a2f844389ae6c
Drew Ellingson <drew.ellingson@gmail.com>
2020-12-08 21:45:02 -0800

    removing day 8 var names from day 9

--- day_09/soln.py
+++ day_09/soln.py
@@ -2,32 +2,32 @@ def parse(in_file):
     return [int(x) for x in in_file.readlines()]
 
 
-def is_valid(instr, indx, preamble_len):
+def is_valid(nums, indx, preamble_len):
     if indx < preamble_len:
         return True
-    curr = instr[indx]
-    previous = instr[indx - preamble_len : indx]
+    curr = nums[indx]
+    previous = nums[indx - preamble_len : indx]
     sum_pairs = [(x, y) for x in previous for y in previous if x + y == curr and x != y]
     return len(sum_pairs) != 0
 
 
-def p1(instr, preamble_len):
-    instr_valid = list(
-        map(lambda x: is_valid(instr, x, preamble_len), range(len(instr)))
+def p1(nums, preamble_len):
+    num_valid = list(
+        map(lambda x: is_valid(nums, x, preamble_len), range(len(nums)))
     )
-    return instr[instr_valid.index(False)]
+    return nums[num_valid.index(False)]
 
 
-def p2(instr, preamble_len):
-    inv_num = p1(instr, preamble_len)
+def p2(nums, preamble_len):
+    inv_num = p1(nums, preamble_len)
     done = False
-    for i in range(len(instr)):
+    for i in range(len(nums)):
         if done:
             break
-        base_val = instr[i]
+        base_val = nums[i]
         sum_nums = [base_val]
-        for j in range(len(instr))[i + 1 :]:  # subsequent numbers in list
-            add_val = instr[j]
+        for j in range(len(nums))[i + 1 :]:  # subsequent numbers in list
+            add_val = nums[j]
             sum_nums.append(add_val)
             if sum(sum_nums) == inv_num:
                 done = True
@@ -42,8 +42,8 @@ def p2(instr, preamble_len):
 if __name__ == "__main__":
     # with open('input_test_1.txt') as my_file:
     with open("input.txt") as my_file:
-        instr = parse(my_file)
+        nums = parse(my_file)
     preamble_len = 25
 
-    print(f"P1 Answer: {p1(instr, preamble_len)}")
-    print(f"P2 Answer: {p2(instr, preamble_len)}")
+    print(f"P1 Answer: {p1(nums, preamble_len)}")
+    print(f"P2 Answer: {p2(nums, preamble_len)}")
